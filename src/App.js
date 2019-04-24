33420beb9de7ca65371cb2f5dd0dcc9ec5819597
Vterebenin <niceserver11@mail.ru>
2019-04-25 00:02:01 +0300

    rewrite the canvas mechanic to PatternLock.js(doing some upgrade on this lib)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react"
-
-
+import PatternLock from './patternLock';
 
 class Runefield extends Component {
 
@@ -10,116 +9,45 @@ class Runefield extends Component {
       listening: false
     }
     this.coords = [];
-
+    this.spellbook = {
+      fireball: 123,
+      iceshard: 169873,
+      healingWard: 1234,
+      healingWard2: 1237,
+    }
   }
 
   componentDidMount() {
-    this.canvas = this.refs.myCanvas;
-    this.ctx = this.canvas.getContext("2d");
-    this.ctxCoor = this.refs.myCanvas.getBoundingClientRect();
-    let ctx = this.canvas.getContext("2d");
-    this.raf = "";
-    this.sline = {
-      x: 100,
-      y: 100,
-      vx: 5,
-      vy: 1,
-      radius: 25,
-      color: 'blue',
-      draw(fromX, fromY) {
-        ctx.fillStyle = this.color;
-        ctx.beginPath();
-        ctx.moveTo(fromX, fromY);
-        ctx.lineTo(this.x, this.y);
-        ctx.lineWidth = 2;
-        ctx.stroke();
-      }
-    }
-    this.historyObj = {
-      history: [],
-      canvas: null
-    }
+
+    this.lock = new PatternLock("#patternContainer");
+
+    Object.keys(this.spellbook).map(key => {
+
+      console.log(key, this.spellbook[key]);
+      let number = `${this.spellbook[key].toString()}`;
+      console.log(number);
+      this.lock.checkForPattern(number, () => {
+        alert(`You cast your spell: ${key}!`);
+      }, () => {
+        alert("You have failed! take 2 damage");
+      });
+    })
 
   }
 
-  clear() {
-    // let reloadData = JSON.parse(this.historyObj.history[0]);
-    // let ctx = this.historyObj.canvas.getContext('2d');
-    // ctx.putImageData(reloadData, 0, 0);
-  }
-
-  drawLine(x_first, y_first, x_second, y_second) {
-    this.ctx.beginPath();
-    this.ctx.moveTo(x_first, y_first);
-    this.ctx.lineTo(x_second, y_second);
-    this.ctx.lineWidth = 2;
-    this.ctx.stroke();
-  }
-
-  drawCircle(x, y, radius) {
-    this.ctx.beginPath();
-    this.ctx.arc(x, y, radius, 0, Math.PI * 2, false);
-    this.ctx.closePath();
-    this.ctx.fill();
-  }
-
-  getCoords(e) {
-
-    // this.historyObj.canvas = this.canvas;
-    // let historyCtx = this.historyObj.canvas.getContext('2d');
-    // let data = JSON.stringify(historyCtx.getImageData(0, 0, this.historyObj.canvas.width, this.historyObj.canvas.height));
-    // console.log(data);
-
-    
-
-    this.drawCircle(e.clientX - this.ctxCoor.left, e.clientY - this.ctxCoor.top, 3);
-    // this.historyObj.history.push(data);
-    console.log(e.clientX, e.clientX + this.ctxCoor.left);
-    let x1, x2, y1, y2;
-    this.coords.push(e.clientX - this.ctxCoor.left, e.clientY - this.ctxCoor.top);
-    console.log(this.coords);
-
-    if (this.coords.length === 4) {
-
-      x1 = this.coords[0]; x2 = this.coords[2];
-      y1 = this.coords[1]; y2 = this.coords[3];
-
-      var perpX = (x1 + x2) / 2;
-      var perpY = (y1 + y2) / 2;
 
 
-      this.drawCircle(perpX, perpY, 3);
-      this.drawLine(this.coords[0], this.coords[1], this.coords[2], this.coords[3]);
-      this.coords = [];
 
-      this.canvas.onmousemove = (e) => {
-        this.clear();
-        this.sline.x = e.clientX;
-        this.sline.y = e.clientY;
-        this.sline.draw(this.coords[0], this.coords[1]);
-      }
-
-    }
-  }
-
-  preCastLine(e) {
-
-  }
-
-  restoreCanvas() {
-
-  }
 
 
   render() {
     return (
-      <canvas
-        ref="myCanvas"
-        onMouseDown={(e) => this.restoreCanvas(e)}
-        onClick={(e) => this.getCoords(e)}
-        style={{ "border": "1px solid #c6c6c6" }}
-        width="500" height="500">
-      </canvas>
+      <div>
+
+        <div id="patternContainer">
+
+        </div>
+      </div>
     )
   }
 }
